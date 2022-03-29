3970c099d53ff5f9c0c8d8d08d6294ba8138dcd1
Erik Golinelli <erik@codekraft.it>
2022-03-29 21:11:03 +0200

    updated demo

--- index.php
+++ index.php
@@ -1,38 +1,79 @@
-<?php
-echo '<pre>';
+<!doctype html>
+<html lang="EN">
+<head>
+    <meta charset="utf-8">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <style>
+      body {
+        font-family: monospace;
+      }
+      body > div {
+        max-width: 1200px;
+        margin: auto;
+      }
+      div:nth-of-type(2n) {
+        background: #f3f3f3;
+      }
+      pre {
+        padding: 20px;
+        background: #333;
+        color: white;
+        font-size: 10px;
+        line-height: 1.3;
+        white-space: pre-wrap;
+      }
+      img,
+      video,
+      audio {
+        width: 200px;
+        height: 200px;
+        object-fit: contain;
+        padding: 10px;
+        margin: 10px 0;
+        border: 1px solid #ccc;
+      }
+    </style>
+</head>
+
+<body>
+
+<?php
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 require_once('filemeta.php');
 
-echo '<br/><br/><br/>';
+foreach (array_slice(scandir(__DIR__.'/media'), 2) as $media) {
 
-$image_blob = new Filemeta(__DIR__.'/arrow.avif');
-print_r($image_blob->extract_meta());
-echo '<br/><br/><br/>';
+    $filepath = __DIR__.'/media/'.$media;
 
+    $mime = explode("/",mime_content_type('./media/'.$media));
 
-$image_blob = new Filemeta(__DIR__.'/arrow.webp');
-print_r($image_blob->extract_meta());
-echo '<br/><br/><br/>';
+    echo '<div class="media-wrap">';
 
-$image_blob2 = new Filemeta(__DIR__.'/arrow.jpg');
-print_r($image_blob2->extract_meta());
-echo '<br/><br/><br/>';
+    switch ($mime[0]) {
+        case "image":
+            echo "<img src=\"media/$media\" />";
+            break;
+        case "video":
+            echo "<video controls><source src=\"media/$media\" /></video>";
+            break;
+        case "audio":
+            echo "<audio controls><source src=\"media/$media\" /></audio>";
+            break;
+    }
 
-$image_blob2 = new Filemeta(__DIR__.'/icc.jpg');
-print_r($image_blob2->extract_meta());
-echo '<br/><br/><br/>';
+    printf("<p>%s <span>%s %s</span> (%s)</p>", $media, $mime[0], $mime[1], $filepath);
 
-$image_blob2 = new Filemeta(__DIR__.'/Nikon_D70.jpg');
-print_r($image_blob2->extract_meta());
-echo '<br/><br/><br/>';
-
-
-$image_blob2 = new Filemeta(__DIR__.'/thumbs-up.apng');
-print_r($image_blob2->extract_meta());
-
-
-echo '</pre>';
+    echo "<pre>";
+    $image_blob = new Filemeta($filepath);
+    print_r($image_blob->extract_meta());
+    echo '</pre></div><br/>';
+}
+?>
+</body>
+</html>
