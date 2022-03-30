34dc7161b8515bd98d0b31fdda82e3b09fd7b3e7
Erik Golinelli <erik@codekraft.it>
2022-03-30 12:34:45 +0200

    riffs headers - parse header to detect the type and basi metata

--- filemeta.php
+++ filemeta.php
@@ -101,6 +101,215 @@ class Filemeta {
         return "you are looking for signature that start with '$signature' or this mime is unknown";
     }
 
+
+    private function decodeLossyChunkHeader( $header ) {
+        // Bytes 0-3 are 'VP8 '
+        // Bytes 4-7 are the VP8 stream size
+        // Bytes 8-10 are the frame tag
+        // Bytes 11-13 are 0x9D 0x01 0x2A called the sync code
+        $syncCode = substr( $header, 11, 3 );
+        if ( $syncCode != "\x9D\x01\x2A" ) {
+            $this->metadata['errors']['VP8 '] = 'WebP decodeLossyChunkHeader Invalid sync code: '. bin2hex( $syncCode );
+        }
+
+        // Bytes 14-17 are image size
+        $imageSize = unpack( 'v2', substr( $header, 14, 4 ) );
+        // Image sizes are 14 bit, 2 MSB are scaling parameters which are ignored here
+        $this->metadata += array(
+            'compression' => 'lossy',
+            'width' => $imageSize[1] & 0x3FFF,
+            'height' => $imageSize[2] & 0x3FFF
+        );
+    }
+
+    private function decodeLosslessChunkHeader( $header ) {
+        // Bytes 0-3 are 'VP8L'
+        // Bytes 4-7 are chunk stream size
+        // Byte 8 is 0x2F called the signature
+        if ( $header[8] != "\x2F" ) {
+            $this->metadata['errors']['VP8L'] =  'Invalid signature: ' . bin2hex( $header[8] );
+        }
+
+        // Bytes 9-12 contain the image size
+        // Bits 0-13 are width-1; bits 15-27 are height-1
+        $imageSize = unpack( 'C4', substr( $header, 9, 4 ) );
+
+        $this->metadata += array(
+            'compression' => 'lossless',
+            'width'       => ( $imageSize[1] | ( ( $imageSize[2] & 0x3F ) << 8 ) ) + 1,
+            'height'      => ( ( ( $imageSize[2] & 0xC0 ) >> 6 ) | ( $imageSize[3] << 2 ) | ( ( $imageSize[4] & 0x03 ) << 10 ) ) + 1
+        );
+    }
+
+    private function decodeExtendedChunkHeader( $header ) {
+        // Bytes 0-3 are 'VP8X'
+        // Byte 4-7 are chunk length
+        // Byte 8-11 are a flag bytes
+        $flags = unpack( 'c', substr( $header, 8, 1 ) );
+
+        // Byte 12-17 are image size (24 bits)
+        $width  = unpack( 'V', substr( $header, 12, 3 ) . "\x00" );
+        $height = unpack( 'V', substr( $header, 15, 3 ) . "\x00" );
+
+        $this->metadata += array(
+            'compression'  => 'unknown',
+            'animated'     => ( $flags[1] & VP8X_ANIM ) == VP8X_ANIM,
+            'transparency' => ( $flags[1] & VP8X_ALPHA ) == VP8X_ALPHA,
+            'EXIF'         => ( $flags[1] & VP8X_EXIF ) == VP8X_EXIF,
+            'ICC'          => ( $flags[1] & VP8X_ICC ) == VP8X_ICC,
+            'XMP'          => ( $flags[1] & VP8X_XMP ) == VP8X_XMP,
+            'width'        => ( $width[1] & 0xFFFFFF ) + 1,
+            'height'       => ( $height[1] & 0xFFFFFF ) + 1
+        );
+    }
+
+    private function decodeExifChunkHeader($img_metadata) {
+        // EXIF
+        // TODO: here the first bug! sometimes the exif header is jfif like and needs to be parsed in the "old" fashioned way (TLDR. it's shifted of 4byte)
+        $header_format = 'A4type/' . // get 4 char
+                         'I1size/';  // get 1 int
+
+        $header = unpack($header_format, substr( $img_metadata, 0, 8 ));
+
+        // fetch header in order to find "0000008" that marks the beginning of exif data before the idf count (what we need)
+        $meta_chunk = unpack( 'H40', substr( $img_metadata, 8, 20 ) )[1];
+
+        $exif_start = strpos($meta_chunk, "00000008");
+        $exif_start_shift = ($exif_start === 8 ) ? 8 : 8 + (($exif_start - 8) * .5);
+
+        $header_riff_format =
+            'A2byte_order/' . // 2byte get 4 string |  "II" (4949.H) (little endian) or "MM" (4D4D.H) (big endian)
+            'H4fixed42/' . // 2byte get 4 string | magic number 42 fixed 002A.h
+            'H8offset/'.  // 4byte get 4 string | 0th IFD offset. If the TIFF header is followed immediately by the 0th IFD, it is written as 00000008.H.
+            'H*idf_count/';  // the count of identifiers to read in the next function
+
+        $metadata = array_merge(
+            $header,
+            unpack($header_riff_format, substr( $img_metadata, $exif_start_shift, 10 )),
+            array('orientation' => '')
+        );
+
+        // TODO: has to be checked if correct... the reason is that value needs to be decoded from hex with some rules
+        // (following the description of the field)
+        // The number of values. It should be noted carefully that the count is not the sum of the bytes. In the case of one
+        // value of SHORT (16 bits), for example, the count is '1' even though it is 2 bytes.
+        $metadata['idf_count'] = hexdec($metadata['idf_count']);
+
+        for ( $i = 0; $i <= $metadata['idf_count'] - 1; $i++ ) {
+
+            // Read the next 12 bytes each loop
+            $exif_raw = substr( $img_metadata, 10 + $exif_start_shift + ( 12 * $i ), 12 );
+
+            // Unpack 12bytes as 24hex values into char string
+            $meta_chunk = unpack( 'H24', $exif_raw )[1];
+
+            // Split the hex string into
+            $meta_chunk_tag = substr($meta_chunk, 0, 4);
+            $meta_chunk_offset = hexdec(substr($meta_chunk, 16, 8));
+            $meta_chunk_count = hexdec(substr($meta_chunk, 8, 8)); // the number of values (string length)
+
+            // TODO: If the value is smaller than 4 bytes,
+            // the value is stored in the 4-byte area starting from the left,
+            // i.e., from the lower end of the byte offset area
+            if ($meta_chunk_tag == '0112') {
+                $this->metadata['orientation'] = substr($meta_chunk, 16, 4);
+            }
+
+            // saves the hex decoded data
+            $metadata[$i] = array(
+                'tag' => $meta_chunk_tag,
+                'type' => substr($meta_chunk, 4, 4), // 2bit TYPE: 0-1 Tag | 2-3 type | 4-7 Count | 8-11 value offset
+                'value' => substr( $img_metadata, $meta_chunk_offset, $meta_chunk_count ), // 4bit the item value - 1 byte 8bit uint | 2 ascii 8byte with 7bit ascii code | 3 short 16bit uint | 4 long 32bit uint | 5 rational long/long | 7 undefined 8bit any | 9 SLONG 4byte singed int | 10 SRATIONAL SLONG/SLONG
+                'raw_value_data' => array( 'hex' => $meta_chunk, 'offset' => $meta_chunk_offset, 'count' => $meta_chunk_count),
+            );
+        }
+
+        $this->metadata['EXIF'] = $metadata;
+    }
+
+    /**
+     * @param $img_metadata
+     *
+     * @return void
+     */
+    private function decodeIccpChunkHeader($img_metadata) {
+
+        // ITPC PARSE
+        // https://www.color.org/icc_specs2.xalter
+        // https://www.color.org/specification/ICCSpecRevision_25-02-10_dictType.pdf
+        // https://www.color.org/icc32.pdf (definitions near page 80)
+        $header_format = 'A4type/' . // get 4 string
+                         'I1size/' ; // get 1byte integer
+
+        $metadata['parsed']['header'] = unpack($header_format, substr( $img_metadata, 0, 8 ));
+
+        $metadata['parsed']['raw-header'] = $raw_header = substr( $img_metadata, 8, 128 );
+        $metadata['parsed']['raw-body'] = $raw_body = substr( $img_metadata, 128 );
+
+        $iccp_format = 'Z4tag/' .
+                       'Noffset/' .
+                       'Nlength/';
+
+        for ($i = 1; $i <= 10; $i++) {
+            $parsed_iccp[$i] = unpack($iccp_format, substr( $raw_body, 12*$i, 12 ));
+
+            $parsed_iccp[$i]['data'] = substr(
+                $img_metadata,
+                $parsed_iccp[$i]['offset'] + 8,
+                $parsed_iccp[$i]['length']
+            );
+
+            $metadata["body-$i"] = array(
+                "tag" => substr( $parsed_iccp[$i]['tag'], 0, 4 ),
+                "type" => substr( $parsed_iccp[$i]['data'], 0, 4 ),
+                "data" => substr( $parsed_iccp[$i]['data'], 4 )
+            );
+        }
+
+        $metadata['icc'] = "ICC profile present";
+
+        // check for icc profile
+        if ( substr( $raw_header, 36, 4 ) != 'acsp' ) {
+            $metadata['icc'] = "ICC profile INVALID (no acsp flag) " .substr( $raw_header, 32, 4 );
+        }
+
+        // invalid ICC profile
+        else {
+            $input  = substr( $raw_header, 16, 4 );
+            $output = substr( $raw_header, 20, 4 );
+            $metadata['icc-input'] = 'ICC profile Input: ' . $input;
+            $metadata['icc-output'] = 'ICC profile Output: ' . $output;
+            // Ignore Color profiles for conversion to other color-spaces e.g. CMYK/Lab
+            if ( $input != 'RGB ' || $output != 'XYZ ' ) {
+                $metadata['icc'] = 'ICC profile ignored';
+            }
+        }
+
+        $this->metadata['ICCP'] = $metadata;
+    }
+
+    /**
+     * @param $img_metadata
+     *
+     * @return array
+     */
+    private function decodeXmpChunkHeader($img_metadata) {
+
+        // XMP PROFILE
+        // https://en.wikipedia.org/wiki/Extensible_Metadata_Platform
+        // https://web.archive.org/web/20180919181934/http://www.metadataworkinggroup.org/pdf/mwg_guidance.pdf
+        // https://github.com/jeroendesloovere/xmp-metadata-extractor/blob/master/src/XmpMetadataExtractor.php
+
+        $header_format = 'A4type/' . // get 4 string
+                         'Vsize/'; // get 4 string
+
+        $this->metadata['XMP']['parsed'] = unpack($header_format, substr( $img_metadata, 0, 8 ));
+
+        $this->metadata['XMP']["raw"] = $xmp_raw = utf8_encode(htmlspecialchars(substr( $img_metadata, 8)));
+
+    }
+
+
     /**
      * @param int $maxChunks
      *
